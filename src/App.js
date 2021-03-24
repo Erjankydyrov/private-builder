55ad1d9d70816a7f4377a9f0f854a73f0e2c937c
Erjankydyrov <erjankydyrov29@gmail.com>
2021-03-24 18:12:25 +0600

    corected App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import DangoBuilder from "./components/DangoBuilder/DangoBuilder";
+import Layout from "./components/Layout/Layout";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+        <h1>Dango-builder by Erjan Kydyrov</h1>
+        <DangoBuilder />
+      </Layout>
     </div>
   );
 }

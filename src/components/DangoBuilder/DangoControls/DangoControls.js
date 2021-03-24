7cb153441b856a5b1cd2aecc3a5a5c7fef933282
Erjankydyrov <erjankydyrov29@gmail.com>
2021-03-24 18:17:35 +0600

    added component DangoControls

---
+++ src/components/DangoBuilder/DangoControls/DangoControls.js
@@ -0,0 +1,9 @@
+import classes from "./DangoControls.module.css";
+
+const DangoControls = () => {
+    return ( 
+        <div className = {classes.DangoControls}>Controls</div>
+    );
+}
+ 
+export default DangoControls;

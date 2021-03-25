71f693b79e235ca79a3ced3fc297cab0ef81236b
Erjankydyrov <erjankydyrov29@gmail.com>
2021-03-25 12:03:49 +0600

    corected BunsIngredient

--- src/components/BunsBuilder/BunsIngredient/BunsIngredient.js
+++ src/components/BunsBuilder/BunsIngredient/BunsIngredient.js
@@ -2,9 +2,9 @@ import classes from "./BunsIngredient.module.css";
 
 const BunsIngredient = ({ type }) => {
   const types = {
-    ChocolateBall: { backgroundColor: `red`, width: "35px", height: "35px" },
-    StrawberryBall: { backgroundColor: `blue`, width: "35px", height: "35px" },
-    SharpBall: { backgroundColor: `green`, width: "35px", height: "35px" },
+    ChocolateBall: { backgroundColor: `red`, width: "35px", height: "35px", display: "inline-block" },
+    StrawberryBall: { backgroundColor: `blue`, width: "35px", height: "35px", display: "inline-block" },
+    SharpBall: { backgroundColor: `green`, width: "35px", height: "35px", display: "inline-block" },
   };
 
  

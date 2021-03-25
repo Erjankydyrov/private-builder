75f818c684bbafdee62dc8519f1c3c87117c6cc2
Erjankydyrov <erjankydyrov29@gmail.com>
2021-03-25 12:01:58 +0600

    corected app.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <Layout>
-        <h1>Dango-builder by Erjan Kydyrov</h1>
+        <h1>Buns-builder by Erjan Kydyrov</h1>
         <BunsBuilder />
       </Layout>
     </div>

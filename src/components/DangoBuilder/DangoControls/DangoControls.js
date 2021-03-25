bc8059eba5d4b935693fcabd2170d002fcea35fa
Erjankydyrov <erjankydyrov29@gmail.com>
2021-03-25 11:41:50 +0600

    delete musor

--- src/components/DangoBuilder/DangoControls/DangoControls.js
+++
@@ -1,9 +0,0 @@
-import classes from "./DangoControls.module.css";
-
-const DangoControls = () => {
-    return ( 
-        <div className = {classes.DangoControls}>Controls</div>
-    );
-}
- 
-export default DangoControls;

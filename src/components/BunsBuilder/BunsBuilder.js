e584878aa0486643801b490ca4e0777a942b932a
Erjankydyrov <erjankydyrov29@gmail.com>
2021-03-26 13:26:15 +0600

    added 6 new ingredients

--- src/components/BunsBuilder/BunsBuilder.js
+++ src/components/BunsBuilder/BunsBuilder.js
@@ -4,9 +4,12 @@ import BunsPreview from "./BunsPreview/BunsPreview";
 
 const BunsBuilder = () => {
     const ingredients = {
-        ChocolateBall: 100,
-        StrawberryBall:10,
-        SharpBall: 199,
+        Bun1: 100,
+        Bun2: 10,
+        Bun3: 199,
+        Bun4: 199,
+        Bun5: 199,
+        Bun6: 199,
     };
     return ( 
         <div className = {classes.BunsBuilder}>
--- src/components/BunsBuilder/BunsIngredient/BunsIngredient.js
+++ src/components/BunsBuilder/BunsIngredient/BunsIngredient.js
@@ -2,9 +2,12 @@ import classes from "./BunsIngredient.module.css";
 
 const BunsIngredient = ({ type }) => {
   const types = {
-    ChocolateBall: { backgroundColor: `red`, width: "35px", height: "35px", display: "inline-block" },
-    StrawberryBall: { backgroundColor: `blue`, width: "35px", height: "35px", display: "inline-block" },
-    SharpBall: { backgroundColor: `green`, width: "35px", height: "35px", display: "inline-block" },
+    Bun1: { backgroundColor: `red`, width: "35px", height: "35px", display: "inline-block" },
+    Bun2: { backgroundColor: `blue`, width: "35px", height: "35px", display: "inline-block" },
+    Bun3: { backgroundColor: `green`, width: "35px", height: "35px", display: "inline-block" },
+    Bun4: { backgroundColor: `pink`, width: "35px", height: "35px", display: "inline-block" },
+    Bun5: { backgroundColor: `orange`, width: "35px", height: "35px", display: "inline-block" },
+    Bun6: { backgroundColor: `yellow`, width: "35px", height: "35px", display: "inline-block" },
   };
 
  

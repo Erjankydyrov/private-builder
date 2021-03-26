7d7f08b74ec59c1480d3cd195c3474763d254d15
Erjankydyrov <erjankydyrov29@gmail.com>
2021-03-26 14:36:42 +0600

    corected bunsControl

--- src/components/BunsBuilder/BunsControls/BunsControl/BunsControl.js
+++ src/components/BunsBuilder/BunsControls/BunsControl/BunsControl.js
@@ -2,10 +2,12 @@ import classes from "./BunsControl.module.css";
 
 const BunsControl = ({ type }) => {
     return ( 
-        <div className = {classes.BunsControl}>
-            <button className = {classes.ControlA}>-</button>
-            {type}
-            <button className = {classes.ControlB}>+</button>
+        <div className = {classes.ControlGlav}>
+            <div className = {classes.BunsControl}>
+                <button className = {classes.ControlA}>-</button>
+                {type}
+                <button className = {classes.ControlB}>+</button>
+            </div>
         </div>
     );
 }

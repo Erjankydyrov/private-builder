4c7377e39ffa97fb81cc48a5a2336da381a8a156
Erjankydyrov <erjankydyrov29@gmail.com>
2021-04-06 23:33:52 +0600

    added new component SwitchBun

---
+++ src/components/BunsBuilder/BunsControls/SwitchBun/SwitchBun.js
@@ -0,0 +1,9 @@
+import classes from "./SwitchBun.module.css";
+
+const SwitchBun = () => {
+    return ( 
+        <div className = {classes.SwitchBun}></div>
+    );
+}
+ 
+export default SwitchBun;

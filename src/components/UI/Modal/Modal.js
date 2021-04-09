5261f441e79ac018458c9b51665267f838fb098c
Erjankydyrov <erjankydyrov29@gmail.com>
2021-04-09 14:03:12 +0600

    added news for Modal

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,12 +1,21 @@
 import classes from "./Modal.module.css";
 import Backdrop from "../Backdrop/Backdrop";
+import Button from "../Button/Button";
 
-const Modal = ({ children }) => {
+const Modal = ({ show, children, cancel }) => {
+  const style = {
+    transform: show ? "translateY(0)" : "translateY(-100vh)",
+  };
+
   return (
     <div className={classes.Modal}>
-      <Backdrop show={true} />
-      <div className={classes.content}>
+      <Backdrop show={show} click={cancel} />
+      <div className={classes.content} style={style}>
         {children}
+        <div className={classes.CaseButtons}>
+          <Button onClick={cancel} green="true">Checkout</Button>
+          <Button onClick={cancel} order="true">Cancel</Button>
+        </div>
       </div>
     </div>
   );

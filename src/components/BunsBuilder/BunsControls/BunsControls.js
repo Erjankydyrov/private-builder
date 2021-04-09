796dd282892e155bfc6b662ed4d72689683c0a99
Erjankydyrov <erjankydyrov29@gmail.com>
2021-04-09 13:58:48 +0600

    corected Controls

--- src/components/BunsBuilder/BunsControls/BunsControls.js
+++ src/components/BunsBuilder/BunsControls/BunsControls.js
@@ -3,38 +3,38 @@ import BunsControl from "./BunsControl/BunsControl";
 import classes from "./BunsControls.module.css";
 import SwitchBun from "./SwitchBun/SwitchBun";
 
-
-const BunsControls = ({ switchFilling, filling, addIngredient, removeIngredient }) => {
-  
+const BunsControls = ({
+  switchFilling,
+  startOrdering,
+  filling,
+  addIngredient,
+  ingredients,
+  removeIngredient,
+}) => {
   const result = [];
-  const ingredients = [
-    "PBuns",
-    "Bread",
-    "BBuns",
-    "Crois",
-    "Ecler",
-    "MBuns",
-  ];
+  const names = ["PBuns", "Bread", "BBuns", "Crois", "Ecler", "MBuns"];
 
-  for (const ingredient in ingredients) {
+  for (const name in names) {
     result.push(
       <BunsControl
-        type={ingredients[ingredient] + filling}
-        key={ingredients[ingredient]}
+        type={names[name] + filling}
+        key={names[name]}
         switchFilling={switchFilling}
-        count={ingredients[ingredient]}
+        count={ingredients.length}
         addIngredient={addIngredient}
         removeIngredient={removeIngredient}
       />
     );
-
-    
   }
-  return <div className={classes.BunsControls}>
-    <SwitchBun switchFilling={switchFilling} />
-    {result}
-    <Button disabled={!ingredients.length}>Order</Button>
-    </div>;
+  return (
+    <div className={classes.BunsControls}>
+      <SwitchBun switchFilling={switchFilling} />
+      {result}
+      <Button disabled={!ingredients.length} onClick={startOrdering}>
+        Order
+      </Button>
+    </div>
+  );
 };
 
 export default BunsControls;

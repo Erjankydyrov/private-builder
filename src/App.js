6e15abe27f60048fccc0f873424a5619fe54bca5
Erjankydyrov <erjankydyrov29@gmail.com>
2021-04-16 14:51:41 +0600

    corected

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import { Redirect, Route, Switch } from "react-router";
 import "./App.css";
 import BunsBuilder from "./components/BunsBuilder/BunsBuilder";
+import Checkout from "./components/Checkout/Checkout";
 import Layout from "./components/Layout/Layout";
 
 function App() {
@@ -7,7 +9,11 @@ function App() {
     <div className="App">
       <Layout>
         <h1>Sweet bakery</h1>
-        <BunsBuilder />
+        <Switch>
+          <Route path="/" component={BunsBuilder} exact />
+          <Route path="/checkout" component={Checkout} />
+          <Redirect to="/" />
+        </Switch>
       </Layout>
     </div>
   );
